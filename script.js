ad48827c551ef2f323197a0f9a72e0a6dba509e9
Lasitha Dilan Madhushanka <Lasithadilan940@gmail.com>
2026-02-06 15:50:25 +0530

    Student Registration Form

---
+++ script.js
@@ -0,0 +1,9 @@
+function SignIn(){
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    var lname = document.getElementById("email");
+    var lname = document.getElementById("mobile");
+    var lname = document.getElementById("password");
+    var lname = document.getElementById("cpassword");
+    var lname = document.getElementById("");
+}
